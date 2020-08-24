bc52878356c73d0c5ea1499db9d8690d22e14131
harpreet <Harpreet Kour>
2020-08-24 14:44:05 -0600

    practice

--- CsharpOOP/CsharpOOP/Program.cs
+++ CsharpOOP/CsharpOOP/Program.cs
@@ -21,9 +21,58 @@ namespace CsharpOOP
             // Add a play games method that will decrease energy by 15.
 
 
+            /* Group Task:
+             
+            Add an inaccessible stress level variable, initialized at 0. 
+	            -When homework is done, increase stress by 30. 
+	            -When sleep is done, decrease stress by 50 (or 5 per hour).
+	            -When games are done, decrease stress by 25.
+	            -Add a floor to stress level of 0 (similar to ceiling on energy).
+            Add a query method for stress level and that will output a string based on its level.
+            Throw an exception if stress tries to go over 100.
+            */
+
+            //If you finish: generate a student in main(you can clear out the stuff below this comment), and simulate 10 days for them.IE do homework and games until they're out of energy, sleep and repeat. I know it's a rather simple existence, but it'll suffice for now.
+
+
+
             Student myStudent = new Student();
 
-            
+                Student twoStudent = new Student("Bob", "Jones");
+
+                myStudent.FirstName = "Jane";
+
+                Console.WriteLine(myStudent.StudentID + ": " + myStudent.FirstName + " " + myStudent.LastName);
+
+                Console.WriteLine(twoStudent.StudentID + ": " + twoStudent.FirstName + " " + twoStudent.LastName);
+
+                Console.WriteLine(myStudent.QueryEnergyLevel());
+
+            Student Sarah = new Student("Sarah", "Parker");
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("DAY " + i.ToString());
+                Console.WriteLine(Sarah.QueryStressLeveL());
+                Console.WriteLine(Sarah.QueryEnergyLevel());
+                Sarah.DoHomeWork();
+                Sarah.DoHomeWork();
+                Console.WriteLine(Sarah.QueryStressLeveL());
+                Console.WriteLine(Sarah.QueryEnergyLevel());
+                Sarah.PlayGames();
+                Console.WriteLine(Sarah.QueryStressLeveL());
+                Console.WriteLine(Sarah.QueryEnergyLevel());
+                Sarah.Sleep(12);
+            }
+
+            /*
+            Console.WriteLine(aStudent.QueryStressLeveL());
+            aStudent.DoHomeWork();
+            Console.WriteLine(aStudent.QueryStressLeveL());
+            aStudent.DoHomeWork();
+            aStudent.Sleep();
+            aStudent.Sleep();
+            Console.WriteLine(aStudent.QueryStressLeveL());
+            */
         }
     }
 }
--- CsharpOOP/CsharpOOP/Student.cs
+++ CsharpOOP/CsharpOOP/Student.cs
@@ -7,14 +7,44 @@ namespace CsharpOOP
      public class Student
     {
         // Public properties can be set externally.
-        public int StudentID { get; set; } 
+        public int StudentID { get; set; }
+
+        // A default property auto-generates the backing variable as a private variable of the same type.
+        // The getter and setter are also automatically generated.
+
         public string FirstName { get; set; }
-        public string LastName { get; set; }
-      
+
+        // The above auto-generated property can be likened to the following:
+        private string _lastName;
+        public string LastName
+        {
+            get // Output of the value.
+            {
+                // The "getter" is called whenever the user tries to use the property.
+                // Example: writing it out, assigning another variable to it, etc.
+                // (string myVar = student.LastName)
+                // Do any formatting of values here.
+                return _lastName;
+
+            }
+            set // Input to the value.
+            {
+                // The "setter" is called whenever the user tries to assign the property.
+                // 'value' is a keyword that represents what the user is trying to assign thereto.
+                // Do any validation of values here.
+                _lastName = value;
+
+            }
+        }
+
         // Private properties can only be set in this class.
         private DateTime DateOfBirth { get; set; }
+        // Constructors are used to create an instance of a class.
+        // The class is the schematic and the object is the instance.
 
-        
+        // A "default" constructor takes no parameters. 
+        // If no constructor is declared, C# will generate one that populates properties with their default values. The same is true if any properties go undeclared by the end of the constructor.
+
         // We have to specify the backing variable for EnergyLevel because we are doing some validation with the value. 
         private int _energyLevel;
         private int EnergyLevel
@@ -32,32 +62,117 @@ namespace CsharpOOP
                 }
                 else
                 {
-                    _energyLevel = value;
+                    _energyLevel += value;
                 }
             }
         }
+        private int _stressLevel;
+        private int StressLevel { get; set; }
         public Student() // constructor
         {
             DateOfBirth = DateTime.Now;
             EnergyLevel = 100;
         }
+        // A "partial" constructor takes some of the properties as arguments, and defaults the rest.
+        public Student(string firstName, string lastName)
+        {
+            StudentID = 1000;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = DateTime.Now;
+            EnergyLevel = 100;
+        }
+        // A "greedy" constructor takes all of the properties as arguments.
+        // Depending on your implementation you may or may not allow private properties to be set via parameter. In this example we will not.
+        public Student(int studentID, string firstName, string lastName)
+        {
+            StudentID = studentID;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = DateTime.Now;
+            EnergyLevel = 100;
+        }
+
+        // Additional methods can be defined on a class, and will operate on the instance of the object.
         public void DoHomeWork() //method
         {
-            EnergyLevel -= 25;
+            if (StressLevel > 71)
+            {
+                throw new Exception("Too much stress! Take a break!");
+            }
+            else
+            {
+                EnergyLevel -= 25;
+                // Same as EnergyLevel = EnergyLevel - 25;
+                StressLevel += 30;
+            }
         }
         public void Sleep() //method
         {
             EnergyLevel = 100;
+            StressLevel -= 50;
         }
         public void Sleep(int hours)  //method
         {
             EnergyLevel += hours * 10;
+            StressLevel -= hours * 5;
         }
         public void PlayGames() //method
         {
             EnergyLevel -= 15;
+            StressLevel -= 25;
         }
+        public string QueryEnergyLevel()
+        {
+            string output;
+            if (EnergyLevel > 100)
+            {
+                output = "I'm super well rested and have tons of energy!";
+            }
+            else if (EnergyLevel > 75)
+            {
+                output = "Doing pretty well.";
+            }
+            else if (EnergyLevel > 40)
+            {
+                output = "Starting to get a bit tired.";
+            }
+            else if (EnergyLevel > 10)
+            {
+                output = "Pretty tired. Need rest soon.";
+            }
+            else
+            {
+                output = "Basically a zombie here, why aren't I asleep?";
+            }
 
+            return output;
+        }
+        public string QueryStressLeveL()
+        {
+            string output;
+            if (StressLevel > 100)
+            {
+                output = "I'm super stressed!";
+            }
+            else if (StressLevel > 75)
+            {
+                output = "I'm kinda stressed out";
+            }
+            else if (StressLevel > 40)
+            {
+                output = "Starting to get a bit tired.";
+            }
+            else if (StressLevel > 10)
+            {
+                output = "I feel great";
+            }
+            else
+            {
+                output = "Feeling Great! Ready to do some work!";
+            }
+            return output;
+        }
 
 
 
