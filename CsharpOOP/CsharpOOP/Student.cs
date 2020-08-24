a9caa2731349be7322c1254e4c8e6ae8bcc37db1
harpreet <Harpreet Kour>
2020-08-24 11:46:10 -0600

    Added overload to sleep; Added ceiling to energy; Added independent task

--- CsharpOOP/CsharpOOP/Student.cs
+++ CsharpOOP/CsharpOOP/Student.cs
@@ -4,22 +4,57 @@ using System.Text;
 
 namespace CsharpOOP
 {
-    public class Student
+     public class Student
     {
         // Public properties can be set externally.
         public int StudentID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
+      
         // Private properties can only be set in this class.
         private DateTime DateOfBirth { get; set; }
 
-
+        // We have to specify the backing variable for EnergyLevel because we are doing some validation with the value. 
+        private int _energyLevel;
+        private int EnergyLevel
+        {
+            get
+            {
+                return _energyLevel;
+            }
+            set
+            {
+                // If the incoming value is over 100, clamp it to 100.
+                if (value > 100)
+                {
+                    _energyLevel = 100;
+                }
+                else
+                {
+                    _energyLevel = value;
+                }
+            }
+        }
         public Student()
         {
             DateOfBirth = DateTime.Now;
+            EnergyLevel = 100;
+        }
+        public void DoHomeWork()
+        {
+            EnergyLevel -= 25;
+        }
+        public void Sleep()
+        {
+            EnergyLevel = 100;
+        }
+        public void Sleep(int hours)
+        {
+            EnergyLevel += hours * 10;
         }
 
 
     }
 }
+
+
