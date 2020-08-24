b2aa6ed3ff5e0f802db055977ede16ff197a6d70
harpreet <Harpreet Kour>
2020-08-24 10:37:04 -0600

    added checking number of words in a string program.

---
+++ CsharpOOP/CsharpOOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CsharpOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input, output;
+
+            // In-Class Practice Round 2:
+
+            // Try to do it without any variables at all (remove the line above these comments).
+            // Two lines (prompt and output).
+
+            Console.Write("Please enter a string: ");
+            input = Console.ReadLine();
+
+            output = input.Trim().Contains(' ') ? "Multiple words." : "One word.";
+
+            Console.WriteLine(output);
+        }
+    }
+}
