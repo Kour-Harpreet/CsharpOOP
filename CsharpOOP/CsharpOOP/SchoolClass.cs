4b2655a113edc18c7ebc49e2b9c0285bb1857f9a
harpreet <Harpreet Kour>
2020-08-25 09:23:03 -0600

    added teacher and school class

---
+++ CsharpOOP/CsharpOOP/SchoolClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpOOP
+{
+    class SchoolClass
+    {
+        /*
+       Create Properties for:
+       -Class Code
+       -Class Name
+       -Maximum Students (private)
+      */
+    }
+}
---
+++ CsharpOOP/CsharpOOP/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpOOP
+{
+    class Teacher
+    {
+        /*
+         Create Properties for:
+         -First Name
+         -Last Name
+         -Staff ID
+         -Date of Birth
+        */
+    }
+}
