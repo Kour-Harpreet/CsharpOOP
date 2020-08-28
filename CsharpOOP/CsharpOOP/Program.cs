39ddad0753de22de5d4d6f47cbf12a4f02748a3b
harpreet <Harpreet Kour>
2020-08-28 11:22:39 -0600

    added linq lecture

--- CsharpOOP/CsharpOOP/Program.cs
+++ CsharpOOP/CsharpOOP/Program.cs
@@ -1,74 +1,90 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace CsharpOOP
 {
     class Program
     {
         static void Main(string[] args)
-       
+
         {
-            Student myStudent = new Student();
-            myStudent.PendingHomework.Push(new Homework() { Complexity = 3 });
-            Console.WriteLine(myStudent.WritingInstrument);
-            myStudent.DoHomework();
-            Console.WriteLine(myStudent.WritingInstrument);
+            // LINQ stands for Language INtegrated Query.
+            // It allows us to run "SQL" queries on data structures in C#.
 
-            /*Pen redPen = new Pen()
-            {
-                Brand = "Bic",
-                InkColor = "Red",
-                HasLid = true,
-                MaxInk = 1
-            };
-            Pen bluePen = new Pen()
-            {
-                Brand = "Bic",
-                InkColor = "Blue",
-                HasLid = false,
-                MaxInk = 10
-            };
-            Pen blackPen = new Pen()
-            {
-                Brand = "Pilot",
-                InkColor = "Black",
-                HasLid = false,
-                MaxInk = 150
-            };
-            Pen greenPen = new Pen()
-            {
-                Brand = "Sharpie",
-                InkColor = "Green",
-                HasLid = true,
-                MaxInk = 110
-            };
+            List<int> testList = new List<int>() { 2, 5, 12, 7, 32, 95, 42, 65, 9, 42, 7, 42 };
 
-            // 100, 50, exc (then 50 again)
-            TestPen(redPen);
-            // 100, 95, 70
-            TestPen(bluePen);
-            // 100, 99.666666, 98
-            TestPen(blackPen);
-            // 100, 99.5454545454, 97.272727272727
-            TestPen(greenPen);
+            // We've looked at a couple in passing so far, mostly the aggregate ones.
 
-        }
-        public static void TestPen(Pen toTest)
-        {
-            Console.WriteLine($"This is a {toTest.Brand} {toTest.InkColor} pen which has {toTest.InkLevel}% of its ink remaining.");
-            toTest.Write();
-            Console.WriteLine($"This is a {toTest.Brand} {toTest.InkColor} pen which has {toTest.InkLevel}% of its ink remaining.");
-            try
+            // .Max() and .Min() are sort of the equivalent of MAX() and MIN() in SQL.
+            Console.WriteLine($"The largest number is {testList.Max()} and the smallest number is {testList.Min()}.");
+
+            // .Average() is sort of equivalent to AVG() in SQL.
+            Console.WriteLine($"The average number is {testList.Average()}.");
+
+            // .Sum() is sort of equivalent to SUM() in SQL.
+            Console.WriteLine($"The sum is {testList.Sum()}.");
+
+            // .All() will return a boolean which will indicate whether all of the items satisfy a condition (lambda).
+            Console.WriteLine($"All numbers are greater than 1: {testList.All(x => x > 1)}.");
+            Console.WriteLine($"All numbers are greater than 50: {testList.All(x => x > 50)}.");
+
+            // .Any() will return a boolean which will indicate whether any of the items satisfy a condition (lambda).
+            Console.WriteLine($"Any number is less than 1: {testList.Any(x => x < 1)}.");
+            Console.WriteLine($"Any number is greater than 50: {testList.Any(x => x > 50)}.");
+
+            // .Count() will return a number of the items that satisfy a condition (lambda).
+            Console.WriteLine($"Number of items: {testList.Count()}.");
+            Console.WriteLine($"Number of items greater than 20: {testList.Count(x => x > 20)}.");
+            Console.WriteLine($"Number of items less than 50: {testList.Count(x => x < 50)}.");
+
+            // .Distinct() will return the unique items, no duplicates. It typically preserves the first occurance in terms of order. Other methods can be chained thereafter to operate on the distinct items.
+            Console.WriteLine($"Number of distinct items: {testList.Distinct().Count()}.");
+            Console.WriteLine($"Number of distinct items greater than 20: {testList.Distinct().Count(x => x > 20)}.");
+            Console.WriteLine($"Number of distinct items less than 50: {testList.Distinct().Count(x => x < 50)}.");
+
+            // .OrderBy() will reorder the list based on the lambda comparison provided.
+            Console.Write("List in order: ");
+            foreach (int item in testList.OrderBy(x => x))
             {
-                toTest.Write(50);
+                Console.Write(item + " ");
             }
-            catch (Exception ex)
+            Console.WriteLine();
+
+            Console.Write("Distinct list in order: ");
+            foreach (int item in testList.Distinct().OrderBy(x => x))
             {
-                Console.WriteLine(ex.Message);
+                Console.Write(item + " ");
             }
-            Console.WriteLine($"This is a {toTest.Brand} {toTest.InkColor} pen which has {toTest.InkLevel}% of its ink remaining.");
-*/
+            Console.WriteLine();
+
+            // .Select() will return a sub-list of associated values based on the lambda.
+            Console.Write("List in order, plus 1: ");
+            foreach (int item in testList.Select(x => x + 1).OrderBy(x => x))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            // .Where() will return a sub-list of filtered values based on the lambda.
+            Console.Write("List in order, only over 50: ");
+            foreach (int item in testList.Where(x => x > 50).OrderBy(x => x))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("List in order, only evens: ");
+            foreach (int item in testList.Where(x => x % 2 == 0).OrderBy(x => x))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            // average, distinct, where, select
+            // Find the average of the distinct values over 25, but subtract 5 from each before the average takes place.
+            Console.WriteLine($"Answer: {testList.Where(x => x > 25).Distinct().Select(x => x - 5).Average()}");
 
         }
     }
