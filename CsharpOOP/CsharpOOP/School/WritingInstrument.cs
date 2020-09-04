14b11fb75e9af1a8f1ebeb6b070198eb252d6869
harpreet <Harpreet Kour>
2020-09-04 10:54:52 -0600

    last commit

--- CsharpOOP/CsharpOOP/School/WritingInstrument.cs
+++ CsharpOOP/CsharpOOP/School/WritingInstrument.cs
@@ -12,7 +12,8 @@ namespace CsharpOOP.School
           
             Set Pen to inherit from WritingInstrument.
             
-            Create a Pencil class that has a similar behaviour to Pen, except uses a GraphiteRemaining property rather than ink level.
+            Create a Pencil class that has a similar behaviour to Pen, 
+        except uses a GraphiteRemaining property rather than ink level.
                 GraphiteRemaining will start at 100%, and decrease by 0.1% per character written.
             Set the WritingInstrument property in Student to be polymorphic.
          */
