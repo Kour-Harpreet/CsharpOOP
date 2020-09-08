958f88762d96ff48dedd857da5d659b8d8ff5d55
harpreet <Harpreet Kour>
2020-09-07 21:10:17 -0600

    Colour notes;

--- CsharpOOP/CsharpOOP/Colour.cs
+++ CsharpOOP/CsharpOOP/Colour.cs
@@ -6,6 +6,10 @@ namespace CsharpOOP
 {
     public struct Colour
     {
+        // We're declaring an enumerator here, which we use in each shape.
+        // It's not an object reference, but rather a value of this enumerator type.
+        // This allows us to compare the colours of multiple shapes, because the comparison is handled by this enumerator.
+        // If each shape had its own enumerator declaration, C# wouldn't know how to implicitly compare them.
         public enum ColourValue
         {
             Red,
