aefb06437953d3402b58778c77b1f2dc47c59016
harpreet <Harpreet Kour>
2020-09-07 21:00:28 -0600

    Example of comparing all properties manually;

--- CsharpOOP/CsharpOOP/Program.cs
+++ CsharpOOP/CsharpOOP/Program.cs
@@ -15,6 +15,11 @@ namespace CsharpOOP.Shapes
             // Functionally the same as "myFirstShape == mySecondShape" except Microsoft decided not to include that default operator on struct.
             // Structs are compared by their properties, and not by their object reference. This means two structs with the same properties are treated as equal, unlike class/objects.
             Console.WriteLine(myFirstShape.Equals(mySecondShape));
+            // Comparing all properties can take a lot of space.
+            Console.WriteLine(myFirstShape.Area == mySecondShape.Area && myFirstShape.Circumference == mySecondShape.Circumference && myFirstShape.Colour == mySecondShape.Colour && myFirstShape.Perimeter == mySecondShape.Perimeter);
         }
+
     }
 }
+
+        
